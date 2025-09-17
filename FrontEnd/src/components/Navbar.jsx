b41391533a15605fcedb010aa5ab3a506086b360
Amjad-Bin-Aslam <amjadbinaslam604@gmail.com>
2025-09-18 01:48:47 +0500

    added

--- FrontEnd/src/components/Navbar.jsx
+++ FrontEnd/src/components/Navbar.jsx
@@ -1,17 +1,19 @@
 import React, { useState } from 'react'
 import { assets } from '../assets/assets'
-import { Link, NavLink } from 'react-router-dom'
+import { Link, NavLink, useNavigate } from 'react-router-dom'
+
 
 function Navbar() {
 
   const [visible , setVisible] = useState(false);
+  const navigate = useNavigate()
 
   return (
 
     <div className="flex items-center justify-between py-5 font-medium">
 
       {/* Logo */}
-      <img className="w-36" src={assets.logo} alt="Logo" />
+      <img className="w-36 cursor-pointer" src={assets.logo} alt="Logo" onClick={() => navigate('/')}/>
 
       {/* Navigation Links */}
       <ul className="hidden sm:flex gap-5 text-sm text-gray-700">
@@ -70,7 +72,7 @@ function Navbar() {
               <p className='cursor-pointer hover:text-black'>My Profile </p>
               <p className='cursor-pointer hover:text-black'>Orders</p>
               <p className='cursor-pointer hover:text-black'>Logout</p>
-              </div>
+              </div> 
             </div>
 
           </div>
@@ -81,6 +83,20 @@ function Navbar() {
             <img className='w-5 cursor-pointer sm:hidden' src={assets.menu_icon} alt="" 
             onClick={()=>setVisible(true)}
             />
+      </div>
+
+      {/* Sidebar menu for small screen */}
+      <div className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white transition-all ${visible ? 'w-full' : 'w-0'}`}>
+            <div className='flex flex-col text-gray-600'>
+                <div onClick={()=> setVisible(false)} className='flex items-center gap-4 p-3 cursor-pointer'>
+                    <img className='h-4 rotate-180' src={assets.dropdown_icon} alt="" />
+                    <p>Back</p>
+                </div>
+                <NavLink onClick={()=> setVisible(false)} className='py-2 pl-6 border' to='/'>HOME</NavLink>
+                <NavLink onClick={()=> setVisible(false)} className='py-2 pl-6 border' to='/collection'>COLLECTION</NavLink>
+                <NavLink onClick={()=> setVisible(false)} className='py-2 pl-6 border' to='/about'>ABOUT</NavLink>
+                <NavLink onClick={()=> setVisible(false)} className='py-2 pl-6 border' to='/contact'>CONTACT</NavLink>
+            </div>
       </div>
 
     </div>
--- FrontEnd/src/pages/Home.jsx
+++ FrontEnd/src/pages/Home.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import Hero from '../components/Hero'
+import LatestCollection from '../context/LatestCollection'
+import BestSeller from '../components/BestSeller'
 
 function Home() {
   return (
     <div>
-      
+      <Hero />
+      <LatestCollection />
+      <BestSeller />
     </div>
   )
 }
