1fc6117f7f1ccfb442e62f8d05fe74bce7ece73f
Amjad-Bin-Aslam <amjadbinaslam604@gmail.com>
2025-09-22 00:10:57 +0500

    added

--- FrontEnd/src/pages/Login.jsx
+++ FrontEnd/src/pages/Login.jsx
@@ -4,8 +4,12 @@ function Login() {
 
   const [currentState , setCurrentState] = useState('Sign Up')
 
+  const onSubmitHandler = async (e) =>{
+      e.preventDefault();
+  }
+
   return (
-    <form className='flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-800'>
+    <form className='flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-800' onSubmit={onSubmitHandler}>
 
         <div className='inline-flex items-center gap-2 mb-2 mt-10'>
             <p className='prata-regular text-3xl'>{currentState}</p>
@@ -23,8 +27,8 @@ function Login() {
             ? <p onClick={()=>setCurrentState('Sign Up')} className='cursor-pointer'>Create Account</p>
             : <p onClick={()=>setCurrentState('Login')} className='cursor-pointer'>Login Here</p>
           }
-        </div>
-
+        </div> 
+            <button className='bg-black text-white font-light px-8 py-2 mt-4 cursor-pointer'>{currentState === 'Login' ? 'Sign In' : 'Sign Up'}</button>
     </form>
   )
 }
