00df15d8497c2360d3e4f803ce8ae40cca294282
Amjad-Bin-Aslam <amjadbinaslam604@gmail.com>
2025-09-27 23:27:49 +0500

    added

--- FrontEnd/src/pages/PlaceOrder.jsx
+++ FrontEnd/src/pages/PlaceOrder.jsx
@@ -77,17 +77,6 @@ function PlaceOrder() {
             toast.error(response.data.message)
           }
           break;
-        
-        case 'stripe':
-          const responseStripe = await axios.post(backendUrl + '/api/order/stripe' , orderData ,{headers: {token}})
-          if(response.data.success){
-            const {session_url} = responseStripe.data
-            window.location.replace(session_url)
-          } else {
-            toast.error(responseStripe.data.message)
-          }
-
-          break;
 
         default:
           break;
