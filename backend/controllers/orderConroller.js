dd8f4703527ba953cb0a85392b107f133806beda
Amjad-Bin-Aslam <amjadbinaslam604@gmail.com>
2025-09-27 23:20:21 +0500

    added

--- backend/controllers/orderConroller.js
+++ backend/controllers/orderConroller.js
@@ -1,6 +1,13 @@
 import orderModel from '../models/orderModel.js'
 import userModel from '../models/userModel.js'
+import Stripe from 'stripe'
 
+// Global variables
+const currency = '$'
+const deliveryCharge = 10
+
+// Gateway initialize
+const stripe = new Stripe(process.env.STRIPE_SECRETE_KEY)
 
 // Placing orders using COD method
 const placeOrder =  async (req , res) => {
@@ -37,6 +44,60 @@ const placeOrder =  async (req , res) => {
 // Placing orders using COD method
 const placeOrderStripe =  async (req , res) => {
     
+    try {
+
+        const {userId , items, amount, address} = req.body;
+        const { origin } = req.headers;
+
+        const orderData = {
+            userId,
+            items,
+            amount,
+            address,
+            paymentMethod:"Stripe",
+            payment: false,
+            date: Date.now(),
+        };
+
+        const newOrder =  new orderModel(orderData) 
+        await newOrder.save();
+        
+        const line_items = items.map((item) => ({
+            price_data: {
+                currency:currency,
+                product_data: {
+                    name: item.name
+                } ,
+                unit_amount: item.price * 100
+            } ,
+            quantity: item.quantity
+    }))
+
+    line_items.push({
+        price_data: {
+                currency:currency,
+                product_data: {
+                    name: 'Delivery Charge'
+                } ,
+                unit_amount: deliveryCharge * 100
+            } ,
+            quantity: 1
+    })
+
+    const session = await stripe.checkout.sessions.create({
+        success_url: `${origin}/verify?success=true&orderId=${newOrder._id}`,
+        cancel_url: `${origin}/verify?success=false&orderId=${newOrder._id}`,
+        line_items,
+        mode: 'payment'
+    })
+
+    res.json({ success: true , session_url: session.url })
+
+    } catch (error) {
+        console.log(error)
+        res.json({ success: false, message: error.message })
+    }
+
 }
 
 
